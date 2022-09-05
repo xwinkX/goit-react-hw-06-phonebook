508d190327f5e94ca6ae320dd4afcb72141df367
xwinkX <opalyuk.e88@ukr.net>
2022-09-05 23:37:26 +0300

    sss

--- src/index.js
+++ src/index.js
@@ -8,12 +8,10 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
         <App />
-      </PersistGate>
-    </Provider>
+      </Provider>
+    </PersistGate>
   </React.StrictMode>
 );
-
-export default persistor;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { storage } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import persistReducer from 'redux-persist/es/persistReducer';
 import persistStore from 'redux-persist/es/persistStore';
 import { contactUser } from './contactUser/contactUser';
@@ -7,11 +7,12 @@ import { contactUser } from './contactUser/contactUser';
 const persistConfig = {
   key: 'contact',
   storage,
+  // whitelist: ['items'],
 };
 
 const persistedContactreducer = persistReducer(persistConfig, contactUser);
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     contact: persistedContactreducer.reducer,
   },
@@ -19,4 +20,4 @@ export const store = configureStore({
 
 const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
